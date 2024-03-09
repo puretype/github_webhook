8768f5b5de6b1fdd79dec958cc87d6cd303f9ad8
Matt Whitworth <matt@puretype.ai>
2024-03-09 11:31:17 +0000

    add credo

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule GitHubWebhook.MixProject do
 
   defp deps do
     [
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:plug, "~> 1.4"}
     ]
